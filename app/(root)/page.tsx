c7c7e7fdcc936d0b8f3dbc107cb2df5df7a1c705
Abuaisha-7 <heyrubedru8086@gmail.com>
2025-10-28 15:33:51 +0300

    feching data fro sanity and autogenarate type

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,28 @@
 
 import SearchForm from "@/components/SearchForm";
-import StartupCard from "@/components/StartupCard";
+import StartupCard, { StartupCardType } from "@/components/StartupCard";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({searchParams}: {searchParams: Promise<{query?:string}>}) {
   const query = (await searchParams).query
 
-  const posts = [
-    {
-      _createdAt: new Date(),
-      views:55,
-      author: {_id: 1, name:"Heyru"},
-      _id: 1,
-      description: "this is a description ",
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrWyirjj9k9H364522-sQVS0-5M0-n2KF35w&s",
-      catagory: "robots",
-      title: "we robots",
-    },
-  ];
+  const posts = await client.fetch(STARTUPS_QUERY);
+
+  console.log(posts)
+
+  // const posts = [
+  //   {
+  //     _createdAt: new Date(),
+  //     views:55,
+  //     author: {_id: 1, name:"Heyru"},
+  //     _id: 1,
+  //     description: "this is a description ",
+  //     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrWyirjj9k9H364522-sQVS0-5M0-n2KF35w&s",
+  //     catagory: "robots",
+  //     title: "we robots",
+  //   },
+  // ];
 
   return (
     <>
@@ -37,7 +43,7 @@ export default async function Home({searchParams}: {searchParams: Promise<{query
 </p>
 <ul className="mt-7 card_grid">
 {posts?.length > 0 ? (
-  posts.map((post: StartupCardType, index:number) => (
+  posts.map((post: StartupCardType) => (
     <StartupCard key={post?._id} post={post}/>
   ))
 ) : (
