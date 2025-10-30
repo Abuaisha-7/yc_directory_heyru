bd5645befe39e58b352db396ad921291482be363
Abuaisha-7 <heyrubedru8086@gmail.com>
2025-10-30 14:45:06 +0300

    adding Create Startup Form

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import "easymde/dist/easymde.min.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const workSans = localFont({
   src: [
@@ -68,6 +69,7 @@ export default function RootLayout({
     <html lang="en"  className={`${workSans.variable}`}>
       <body>
         {children}
+        <Toaster />
       </body>
     </html>
   );
