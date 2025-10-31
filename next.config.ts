e4e3ba19edb9e5d75291501587e1cdde01ad07e6
Abuaisha-7 <heyrubedru8086@gmail.com>
2025-10-31 16:34:39 +0300

    corectimg sentry warning

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  productionBrowserSourceMaps: true, // ✅ ensures maps are built
   typescript: {
     ignoreBuildErrors: true,
   },
